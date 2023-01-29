e5aac84ad693c68a06234ad1320b77f8b09cc3a3
Edoardo Nannotti <edonan@gmail.com>
2023-01-29 19:26:08 +0100

    debug: show scanned isbn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
+    <div>{{ isbn }}</div>
     <quagga-scanner @onscan="onscan" ref="quagga" v-if="quagga_visible"></quagga-scanner>
     <button @click="toggle_quagga">{{ quagga_visible ? 'Chiudi lettore ISBN' : 'Leggi ISBN' }}</button>
   </main>
@@ -13,10 +14,12 @@ export default {
   },
   data: () => ({
     quagga_visible: false,
+    isbn: undefined,
   }),
   methods: {
     onscan(code) {
       console.log('scanned:', code)
+      this.isbn = code
     },
     toggle_quagga() {
       this.quagga_visible = !this.quagga_visible

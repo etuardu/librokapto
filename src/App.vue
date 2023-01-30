682f4cd7912f28f6d051155b9bed8cff4556b961
Edoardo Nannotti <edonan@gmail.com>
2023-01-30 22:37:41 +0100

    persistent settings

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router'
 export default {
-  data: () => ({ drawer: null }),
+  data: () => ({
+    drawer: null,
+    nav_items: [
+      { title: 'Library', icon: 'mdi-bookshelf', to: '/' },
+      { title: 'Add books', icon: 'mdi-book-plus', to: '/insert' },
+      { title: 'Settings', icon: 'mdi-cog', to: '/settings' },
+      { title: 'About', icon: 'mdi-information', to: '/about' },
+    ]
+  }),
 }
 </script>
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer">
-      <!--  -->
+      <v-list>
+        <v-list-item
+          v-for="item in nav_items"
+          :key="item.title"
+          :title="item.title"
+          :to="item.to"
+          :icon="item.icon"
+        >
+          <template v-slot:prepend>
+            <v-icon :icon="item.icon"></v-icon>
+          </template>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
---
+++ src/views/SettingsView.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-container>
+
+    <v-text-field
+      label="Google sheets document URL"
+      v-model="doc_url"
+      placeholder="https://docs.google.com/spreadsheets/d/…/edit#gid=0"
+      persistent-placeholder
+    ></v-text-field>
+
+    <v-text-field
+      label="Google sheets Ubicalibri app URL"
+      v-model="app_url"
+      placeholder="https://script.google.com/macros/s/…/exec"
+      persistent-placeholder
+    ></v-text-field>
+
+    <v-select
+      label="Language"
+    ></v-select>
+
+    <v-btn
+      @click="save"
+    >Save settings</v-btn>
+
+    <v-btn
+      text
+      flat
+      size="small"
+      class="ml-6 text-medium-emphasis"
+      @click="copySettingsUrl"
+    >
+      <v-icon class="me-1">mdi-link</v-icon> Share configuration
+    </v-btn>
+
+    <v-alert
+      class="mt-5"
+      :type="alert_type"
+      v-if="alert_visible"
+      variant="tonal"
+      closable
+    >{{ alert_text }}</v-alert>
+
+  </v-container>
+</template>
+<script>
+import { setMany, getMany } from 'idb-keyval'
+export default {
+  data: () => ({
+    doc_url: undefined,
+    app_url: undefined,
+    alert_type: 'success',
+    alert_visible: false,
+    alert_text: '',
+  }),
+  async created() {
+    let search = new URL(window.location.href).searchParams
+    // check if the user reached the page through a shared configuration
+    // In this case the url would contain search parameters.
+    if (search.has('a') && search.has('d')) {
+      this.doc_url = search.get('d')
+      this.app_url = search.get('a')
+      this.alert_text = 'Configuration loaded from shared link. Please save the settings if you want to keep it.'
+      this.alert_visible = true
+      this.alert_type = 'info'
+      return
+    }
+    // otherwise load the values from indexeddb
+    [this.doc_url, this.app_url] = await getMany(['u_gsheet_doc_url', 'u_gsheet_app_url'])
+  },
+  methods: {
+    async save() {
+      this.alert_visible = false
+      try {
+        await setMany([
+          ['u_gsheet_doc_url', this.doc_url],
+          ['u_gsheet_app_url', this.app_url]
+        ])
+        this.alert_type = 'success'
+        this.alert_text = 'Your settings has been updated!'
+        this.alert_visible = true
+      } catch(e) {
+        this.alert_type = 'error'
+        this.alert_text = 'Unable to save settings'
+        this.alert_visible = true
+      }
+    },
+    async copySettingsUrl() {
+      this.alert_visible = false
+      let url = new URL(window.location.href)
+      url.searchParams.append('d', this.doc_url)
+      url.searchParams.append('a', this.app_url)
+      try {
+        await navigator.clipboard.writeText(url);
+        this.alert_type = 'success'
+        this.alert_text = 'Configuration link copied to clipboard!'
+        this.alert_visible = true
+      } catch(e) {
+        this.alert_type = 'error'
+        this.alert_text = 'Cannot copy to clipboard'
+        this.alert_visible = true
+      }
+    }
+  },
+}
+</script>

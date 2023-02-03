109f4b7786fde0bf1048d3787c89a7a754388dcb
Edoardo Nannotti <edonan@gmail.com>
2023-02-03 22:09:01 +0100

    About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
   <v-container>
-    About page
+    <h1 class="text-h5 mb-4">About Librokapto</h1>
+    <p class="text-body-2 my-1">
+    Librokapto – A personal book catalogation app made
+    by Edoardo Nannotti, 2023.
+    </p>
+    <p class="text-body-2 my-1">
+    This software is released under the GPL-3 license.
+    </p>
+    <p class="text-body-2 my-1">
+    For any inquiries please contact <a href="mailto:edonan@gmail.com">edonan@gmail.com</a>.
+    </p>
   </v-container>
 </template>
